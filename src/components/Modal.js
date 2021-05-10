13dfdede87c6111239f01cf9c1c8624cb9e2265a
Alberto González <albertogonzalez.asg@gmail.com>
2021-05-10 11:55:20 -0300

    Updated modal form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,26 +6,34 @@ const Modal  = (props) => {
             <p>{props.modalType}</p>
             <div className='modal-form' id={props.idForm}>
                 <label>Name
-                    <input type='text' placeholder='Go shopping...' />
+                    <input type='text' placeholder='Workout...' required />
                 </label>
                 <label>Description 
-                    <input type='text' placeholder='Meat, vegetables...' />
+                    <input type='text' placeholder='Lift...' />
                 </label>
                 <label>Day
-                    <input type='date' id='datePicker' />
+                    <select>
+                        <option>Monday</option>
+                        <option>Tuesday</option>
+                        <option>Wednesday</option>
+                        <option>Thursday</option>
+                        <option>Friday</option>
+                        <option>Saturday</option>
+                        <option>Sunday</option>
+                    </select>
                 </label>
                 <label>Start
-                    <input type='time' />
+                    <input type='time' required/>
                 </label>
                 <label>End
-                    <input type='time'/>
+                    <input type='time' required/>
                 </label>
                 <label>Color
                     <input type='color'/>
                 </label>
                 <div className='btn-modal-wrapper'>
                     <Button type='btn cancel' text='Cancel' handleClick={props.handleClick} />
-                    <Button type='btn ok' text={props.text} />
+                    <Button type='btn ok' text={props.text} handleClick={props.handleApproval} id={props.idOkBtn} />
                 </div>
             </div>
         </div>
